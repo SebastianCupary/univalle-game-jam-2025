40d9a6045180c22be8a71ad7a6db54305208ec95
SebastianCupary <cvs4003649@est.univalle.edu>
2025-10-18 18:03:44 -0400

    feat: Mecánica de creación de Puentes

---
+++ BridgeReconnection/Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Dictionary<Vector2, Point> AllPoints = new Dictionary<Vector2, Point>();
+
+    private void Awake()
+    {
+        AllPoints.Clear();
+    }
+}
---
+++ BridgeReconnection/Assets/_Project/Scripts/UI/Bar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    public Vector2 StartPosition;
+    public GameObject BarObject;
+    public float maxLength = 1f;
+
+    public void UpdateCreatingBar(Vector2 ToPosition)
+    {
+        transform.position = (ToPosition + StartPosition) / 2;
+
+        Vector2 dir = ToPosition - StartPosition;
+        float angle = Vector2.SignedAngle(Vector2.right, dir); 
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        float length = dir.magnitude;
+        BarObject.transform.localScale = new Vector3(length, BarObject.transform.localScale.y, BarObject.transform.localScale.z); // Adjust the x scale to match the length
+    }
+}
